3c288ecc9b734e10f50dfdbe674920b2dcd8bc3a
gc <chankungdeveloper@gmail.com>
2020-08-04 17:00:05 +0800

    将obj 结构体强转成不同版本下定义的资源结构体

--- go.mod
+++ go.mod
@@ -6,10 +6,13 @@ replace (
 	github.com/coreos/bbolt v1.13.5 => go.etcd.io/bbolt v1.13.5
 	go.etcd.io/bbolt v1.13.5 => github.com/coreos/bbolt v1.13.5
 	go.etcd.io/etcd v0.4.9 => github.com/etcd-io/etcd v3.4.9+incompatible
+	k8s.io/api@v0.18.6 => k8s.io/api v0.18.3
+
 )
 
 require (
 	go.etcd.io/etcd v0.4.9
-	k8s.io/apimachinery v0.18.6
-	k8s.io/kubectl v0.18.6
+	k8s.io/api v0.18.3
+	k8s.io/apimachinery v0.18.3
+	k8s.io/kubectl v0.18.3
 )
--- main/etcdwatcher.go
+++ main/etcdwatcher.go
@@ -17,11 +17,12 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"go.etcd.io/etcd/clientv3"
 	"go.etcd.io/etcd/pkg/transport"
+	res_apps_v1 "k8s.io/api/apps/v1" // 导入 version 根据 创建的资源version决定
+	res_core_v1 "k8s.io/api/core/v1"
 	jsonserializer "k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/kubectl/pkg/scheme"
 	"os"
@@ -75,9 +76,9 @@ func parse(ch clientv3.WatchChan, hook string, verbose bool) {
 		for _, msg := range val.Events {
 
 			if msg.IsCreate() {
-				fmt.Printf("IsCreate events : %v", msg.IsCreate())
+				fmt.Printf("IsCreate events : %v\n", msg.IsCreate())
 			} else if msg.IsModify() {
-				fmt.Printf("IsModify events: %v", msg.IsModify())
+				fmt.Printf("IsModify events: %v\n", msg.IsModify())
 			} else {
 				fmt.Println("delete 事件 跳过")
 				continue
@@ -95,8 +96,8 @@ func parse(ch clientv3.WatchChan, hook string, verbose bool) {
 				fmt.Println(err)
 				os.Exit(255)
 			}
-			fmt.Printf("--------- group: %v --------- version: %v --------- kind: %v ---------\n",
-				gvk.Group, gvk.Version, gvk.Kind)
+			//fmt.Printf("--------- group: %v --------- version: %v --------- kind: %v ---------\n",
+			//	gvk.Group, gvk.Version, gvk.Kind)
 
 			buff := &bytes.Buffer{}
 			err = encoder.Encode(obj, buff)
@@ -113,19 +114,51 @@ func parse(ch clientv3.WatchChan, hook string, verbose bool) {
 			}
 			/*区分资源类型进行具体解析*/
 			switch gvk.Kind {
-			case "Pod":
-				//截取几个比较重要的信息传入hook作为参数
-				podParse(err, buff, hook)
-			case "ReplicaSet":
-
-			case "Endpoint":
-
-			case "Deployment":
-			case "Lease":
-				if _, err = os.Stdout.Write(buff.Bytes()); err != nil {
-					fmt.Println(err)
-					os.Exit(255)
+			case "Event":
+				event, ok := obj.(*res_core_v1.Event)
+				if ok {
+					EventParse(event, hook)
 				}
+				break
+			case "Node":
+				node, ok := obj.(*res_core_v1.Node)
+				if ok {
+					NodeParse(node, hook)
+				}
+			case "Pod":
+				if gvk.Version != "v1" {
+					pod, ok := obj.(*res_core_v1.Pod)
+					if ok {
+						podParse(pod, hook)
+					}
+				}
+				//截取几个比较重要的信息传入hook作为参数
+				break
+			case "ReplicaSet":
+				if gvk.Version != "v1" {
+					rs, ok := obj.(*res_apps_v1.ReplicaSet)
+					if ok {
+						RsParse(rs, hook)
+					}
+				}
+				break
+			case "Endpoints":
+				endpoint, ok := obj.(*res_core_v1.Endpoints)
+				if ok {
+					EndpointParse(endpoint, hook)
+				}
+				break
+			case "Deployment":
+				deploy, ok := obj.(*res_apps_v1.Deployment)
+				if ok {
+					DeployParse(deploy, hook)
+				}
+				break
+			case "Lease":
+				//if _, err = os.Stdout.Write(buff.Bytes()); err != nil {
+				//	fmt.Println(err)
+				//	os.Exit(255)
+				//}
 			default:
 			}
 			//podOption:
@@ -133,52 +166,43 @@ func parse(ch clientv3.WatchChan, hook string, verbose bool) {
 	}
 }
 
-func podParse(err error, buff *bytes.Buffer, hook string) {
-	var jsonMap map[string]interface{}
-	err = json.Unmarshal(buff.Bytes(), &jsonMap)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(255)
-	}
-	podStatus := jsonMap["status"].(map[string]interface{})
-	podMetadata := jsonMap["metadata"].(map[string]interface{})
-	fmt.Printf("podName:%v--------podIP:%v----------podState:[", podMetadata["name"], podStatus["podIP"])
+func NodeParse(node *res_core_v1.Node, hook string) {
 
-	podConditions, ok := podStatus["conditions"].([]interface{})
-	if ok {
-		for _, conditionInterface := range podConditions {
-			condition := conditionInterface.(map[string]interface{})
-			fmt.Printf("type:%v----status:%v,", condition["type"], condition["status"])
+	fmt.Printf("%v,%v\n", node.Name, node.Status)
+}
+
+func EventParse(event *res_core_v1.Event, hook string) {
+	fmt.Printf("event name: %v------ reason: %v \n", event.Name, event.Reason)
+}
+
+func DeployParse(deploy *res_apps_v1.Deployment, hook string) {
+
+}
+
+func EndpointParse(endpoint *res_core_v1.Endpoints, hook string) {
+	fmt.Println("----------", endpoint.Name,endpoint.Name=="")
+
+	for _, subset := range endpoint.Subsets {
+		readyIPs := make([]string, len(subset.Addresses))
+		for index, address := range subset.Addresses {
+			readyIPs[index] = address.IP
 		}
-	}
-	fmt.Printf("],")
-	podContainerStatuses, ok := podStatus["containerStatuses"].([]interface{})
-	if ok {
-		for _, containerStatusInterface := range podContainerStatuses {
-			containerStatus := containerStatusInterface.(map[string]interface{})
-			containerState := containerStatus["state"].(map[string]interface{})
-			for key, value := range containerState {
-				optionState := value.(map[string]interface{})
-				fmt.Printf("%v:{%v}", key, optionState)
-			}
-
-			fmt.Printf("isReady:%v-----image:%v", containerStatus["ready"], containerStatus["image"])
+		notReadyIPs := make([]string, len(subset.Addresses))
+		for index, address := range subset.NotReadyAddresses {
+			notReadyIPs[index] = address.IP
 		}
+		fmt.Printf("-----%v------%v------%v\n", readyIPs, notReadyIPs, subset.Ports)
+	}
+}
 
-	}
-	fmt.Printf("]\n")
-	fmt.Println("*******************************************************************************************************************")
-	podName, ok := podMetadata["name"].(string)
-	if !ok {
-		podName = "nil"
-	}
-	podIP, ok := podStatus["podIP"].(string)
-	if !ok {
-		podIP = "nil"
-	}
+func RsParse(rs *res_apps_v1.ReplicaSet, hook string) {
+
+}
+
+func podParse(pod *res_core_v1.Pod, hook string) {
 	fmt.Println("hooks:", hook)
-	cmd := exec.Command(hook, podName, podIP)
-	err = cmd.Start()
+	cmd := exec.Command(hook, pod.Name, pod.Status.PodIP)
+	err := cmd.Start()
 	if err != nil {
 		fmt.Printf("exec hook scripts failed reason : %v\n", err)
 	}
